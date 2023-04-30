7234ca0ed8e40e26678f32bcd148825f4b025820
Ian Misael Reis <ianmisael@gmail.com>
2023-04-30 20:30:43 -0300

    Projeto Final Curso 1

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from galeria.views import index, imagem
-
+from galeria.views import index, imagem, index_teste, buscar
 
 urlpatterns = [
-    path('', index, name="index"),
-    path('imagem/', imagem, name='imagem'),
+    path('', index, name='index'),
+    path('imagem/<int:foto_id>', imagem, name='imagem'),
+    path('index_teste', index_teste, name='index_teste'),
+    path('buscar', buscar, name='buscar')
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from galeria.models import *
+
+
 
 def index(request):
-    return render(request, 'galeria/index.html')
+    fotografias = Fotografia.objects.order_by("data_fotografia").filter(publicada=True)
+    return render(request, 'galeria/index.html', {"cards": fotografias})
 
-def imagem(request):
-    return render(request, 'galeria/imagem.html')
+def imagem(request, foto_id):
+    fotografia = get_object_or_404(Fotografia, pk=foto_id)
+    return render(request, 'galeria/imagem.html', {"fotografia": fotografia})
+
+
+def index_teste(request):
+    fotografia = Fotografia.objects.order_by('nome').filter(publicada=True)
+    return render(request, 'galeria/index_teste.html', {"cards": fotografia})
+
+def buscar (request):
+    return render(request, "buscar.html")
